a17f4250bd5c055f6e19d173d05cfefe1fc470e8
Oliver Hines <olivermhines@gmail.com>
2024-03-18 15:13:05 -0700

    Fix newtons method error bound

--- examples/newtons.f90
+++ examples/newtons.f90
@@ -12,20 +12,18 @@ program newtons
     x = arg(1)
     n = eintsum(0)
 
-
-
     ! function goes here
     f = x**2-x-1
     ! initial guess goes here
-    guess = 1
+    guess = 2.1
 
     ! a_n = -f(a_(n-1))/f'(a_(n-1))
     ! e_n = f''(a_(n-1))/(2f'(a_(n-1)))*e_(n-1)
     an = 0-populate(f,[lastcalc(0)])/populate(diff(f,1),[lastcalc(0)])
-    err = populate(diff(diff(f,1),1),[lastcalc(0)])/populate(2*diff(f,1),[lastcalc(0)])*lasterr(0)
+    err = populate(diff(diff(f,1),1),[lastcalc(0)])/populate(2*diff(f,1),[lastcalc(0)])*lasterr(0)**2
 
 
-    result = eval(sum(eint(0),infinity,an,err,minn=0,startval=guess,starterr=1._real128),0.000000001_real128)
+    result = eval(sum(eint(0),infinity,an,err,minn=0,startval=guess,starterr=1._real128),0.001_real128)
 
     print*,result
     print'(A)','centered at:'
